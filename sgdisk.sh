06770dc088a32775366276a649712e62d5fe5f95
Justin Malloy <justin.t.malloy@gmail.com>
2021-10-16 16:41:48 -0400

    Update sgdisk.sh

--- sgdisk.sh
+++ sgdisk.sh
@@ -1,25 +1,29 @@
 # format /dev/sda as GPT, GUID Partition Table
 sgdisk - Z /dev/sda
 
-sgdisk -n 0:0:+200M -t 0:ef02 -c 0:"bios_boot" /dev/sda
-sgdisk -n 0:0:+20G -t 0:8300 -c 0:"linux_boot" /dev/sda
-sgdisk -n 0:0:+10G -t 0:8200 -c 0:"linux_swap" /dev/sda
-sgdisk -n 0:0:0 -t 0:8300 -c 0:"os_data" /dev/sda
+sgdisk -n 0:0:+200M -t 0:ef00 -c 0:"boot" /dev/sda
+sgdisk -n 0:0:+10G -t 0:8200 -c 0:"swap" /dev/sda
+sgdisk -n 0:0:0 -t 0:8300 -c 0:"data" /dev/sda
 
 sgdisk -p /dev/sda
 
 # inform the OS of partition changes
 partprobe /dev/sda
+
+mkfs.vfat -n "BOOT" /dev/sda1
+mkswap /dev/sda2 --label "SWAP"
+mkfs.ext4 -n "DATA" /dev/sda3
+
 fdisk -l /dev/sda
 
 # mount & mkdir
 
-mkdir /mnt/bios_boot
-mkdir /mnt/linux_boot
-mkdir /mnt/linux_swap
+mkdir /mnt/boot
+mkdir /mnt/swap
 mkdir /mnt/os_data
 
-mount LABEL=bios_boot /mnt/bios_boot
-mount LABEL=linux_boot /mnt/linux_boot
-mount LABEL=linux_swap /mnt/linux_swap
-mount LABEL=os_data /mnt/os_data
+mount LABEL=DATA /mnt
+mount LABEL=BOOT /mnt/boot
+mount LABEL=SWAP /mnt/swap
+
+swapon /dev/sda2
